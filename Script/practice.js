f45517bdc67366f93fce7a54c4d087842e1eb21e
Jorge Cano <coronero@gmail.com>
2023-03-19 09:14:18 -0700

    Initial commit

---
+++ Script/practice.js
@@ -0,0 +1,14 @@
+//JS vs JQuery
+
+let inputs = document.getElementsByTagName("input");
+console.log(inputs);
+
+// $("input").hide();
+
+// $("#txtEmail").hide();
+
+// $(".form").hide();
+
+// $(".form div:first").hide();
+
+//getting the value from the input password field
---
+++ Script/register.js
@@ -0,0 +1,68 @@
+//User constructor
+function User(e, p, f, l, a, ad, ph, pay, c){
+    this.email = e;
+    this.password = p;
+    this.firstName = f;
+    this.lastName = l;
+    this.age = a;
+    this.address = ad;
+    this.phoneNum = ph;
+    this.paymentMethod = pay;
+    this.color = c;
+}
+
+function isValid(aUser) {
+    let valid = true;
+
+    //reset HTML Style
+    $("#txtEmail").removeClass("error");
+    $("#txtPassWord").removeClass("error");
+    $("#txtFirstName").removeClass("error");
+    $("#txtLastName").removeClass("error");
+
+    //validation if stmnts
+    if(aUser.email === ""){
+        valid = false;
+        $("#txtEmail").addClass("error");
+    }
+    if(aUser.password === ""){
+        valid = false;
+        $("#txtPassWord").addClass("error");
+    }
+    if(aUser.firstName ===""){
+        valid = false;
+        $("#txtFirstName").addClass("error");
+    }
+    if(aUser.lastName ===""){
+        valid = false;
+        $("#txtLastName").addClass("error");
+    }
+    return valid;
+}
+
+function register(){
+    let inputEmail = $("#txtEmail").val();
+    let inputPassword = $("#txtPassWord").val();
+    let inputFirstName = $("#txtFirstName").val();
+    let inputLastName = $("#txtLastName").val();
+    let inputAge = $("#txtAge").val();
+    let inputAddress = $("#txtAddress").val();
+    let inputPhone = $("#txtPhone").val();
+    let inputPayment = $("#selPayment").val();
+    let inputColor = $("#txtColor").val();
+
+    let newUser = new User(inputEmail, inputPassword, inputFirstName,inputLastName, inputAge, inputAddress, inputPhone, inputPayment, inputColor);
+
+    if(isValid(newUser))
+    {
+        console.log(newUser);
+        $("input").val("");
+    }
+    
+}
+    
+function init(){
+    console.log("init");
+}
+
+window.onload=init;
